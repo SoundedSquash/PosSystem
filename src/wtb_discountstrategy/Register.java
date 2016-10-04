ff72a5a78daebd0a49edef2c8bdc7b68014e7fca
wboyer <wboyer@L11103042785.wctc.msft>
2016-10-04 15:05:50 -0500

    Second

---
+++ src/wtb_discountstrategy/Register.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wtb_discountstrategy;
+
+/**
+ *
+ * @author wboyer
+ */
+public class Register {
+
+    public final void startNewSale(String customerId) {
+        
+    }
+
+    public final void addProduct(String productId, int quantity) {
+        
+    }
+
+    public final void endSale() {
+        
+    }
+    
+}
--- src/wtb_discountstrategy/Startup.java
+++ src/wtb_discountstrategy/Startup.java
@@ -3,19 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package possystem;
+package wtb_discountstrategy;
 
 /**
  *
  * @author wboyer
  */
-public class PosSystem {
+public class Startup {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Register register = new Register();
+        
+        //Customer #C100
+        register.startNewSale("C100");
+        register.addProduct("A100",2);
+        register.addProduct("B101",1);
+        register.addProduct("C103",3);
+        register.endSale();
     }
     
 }
