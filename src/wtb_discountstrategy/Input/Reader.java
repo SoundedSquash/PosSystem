472bc65427c9bd26882091310bc4c05960ce3de9
wboyer <wboyer@L11103042785.wctc.msft>
2016-10-05 11:51:25 -0500

    Added Input and Output strategies.

---
+++ src/wtb_discountstrategy/Input/Reader.java
@@ -0,0 +1,7 @@
+package wtb_discountstrategy.Input;
+
+/**
+ * Created by wboyer on 10/5/2016.
+ */
+public interface Reader {
+}
---
+++ src/wtb_discountstrategy/Output/GuiWriter.java
@@ -0,0 +1,7 @@
+package wtb_discountstrategy.Output;
+
+/**
+ * Created by wboyer on 10/5/2016.
+ */
+public class GuiWriter {
+}
